126d7810769c10f07cb97fa61996e9fa030e292b
Luka Barth <lukadbarth@hotmail.com>
2024-11-05 17:49:10 -0300

    first commit

---
+++ src/main/java/com/example/teste_tecnico_lode/TesteTecnicoLodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_tecnico_lode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoLodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoLodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste_tecnico_lode/TesteTecnicoLodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste_tecnico_lode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoLodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
