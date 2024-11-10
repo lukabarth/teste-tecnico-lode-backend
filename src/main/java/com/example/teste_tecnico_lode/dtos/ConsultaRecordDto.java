996d765b2a39747c78f3272c9056b5694b1327c0
Luka Barth <lukadbarth@hotmail.com>
2024-11-10 11:54:51 -0300

    corrige dto

--- src/main/java/com/example/teste_tecnico_lode/dtos/ConsultaRecordDto.java
+++ src/main/java/com/example/teste_tecnico_lode/dtos/ConsultaRecordDto.java
@@ -1,7 +1,6 @@
 package com.example.teste_tecnico_lode.dtos;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public record ConsultaRecordDto(LocalDate dataConsulta, Long agendaId, List<Long> pacienteId) {
+public record ConsultaRecordDto(Long agendaId, List<Long> pacienteId) {
 }
